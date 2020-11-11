3738af2384d122b2f08ab90497326b4c5d62f012
zangtengfei <81709456@qq.com>
2020-11-11 18:09:12 +0800

    first commit add scss and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App:React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello H1</h1>
+        <h2>Hello H2</h2>
+        <h3>Hello H3</h3>
+        <hr/>
+        <code>
+          const a = b
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
