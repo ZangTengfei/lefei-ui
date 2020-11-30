0c0c0f581bf29da29fe7858b709fad81ac1c2ce4
zangtengfei <81709456@qq.com>
2020-11-30 16:04:25 +0800

    完成menu组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
+// import Button, { ButtonType, ButtonSize } from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
-
+import SubMenu from './components/Menu/subMenu'
 const App:React.FC = () => {
   return (
     <div className="App" style={{margin: '20px'}}>
-      <Menu mode='vertical' defaultIndex={0} className="custom-menu" onSelect={(index) => {console.log(index+1)}}>
+      <Menu
+        mode='vertical'
+        defaultIndex={'2-1'} 
+        className="custom-menu" 
+        onSelect={(index) => {console.log(index)}}
+        defaultOpenSubMenus={['2']}
+      >
         <MenuItem>menulink 1</MenuItem>
         <MenuItem disabled>menulink 2</MenuItem>
-        <MenuItem>menulink 3</MenuItem>
+        <SubMenu title='dropdown'>
+          <MenuItem>dropdown1</MenuItem>
+          <MenuItem>dropdown2</MenuItem>
+        </SubMenu>
         <MenuItem>menulink 4</MenuItem>
         <MenuItem>menulink 5</MenuItem>
-        <MenuItem>menulink 6</MenuItem>
-        <MenuItem>menulink 7</MenuItem>
       </Menu>
-
-      <Button onClick={(e) => {alert('123')}}> Hello alert </Button>
-      <Button className="custom"> Hello </Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}> Hello small </Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello large </Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}> Hello large danger </Button>
-      <Button disabled btnType={ButtonType.Link} href="https://baidu.com"> Baidu disabled Link </Button>
-      <Button btnType={ButtonType.Link} href="https://baidu.com"> Link </Button>
-
     </div>
   );
 }
